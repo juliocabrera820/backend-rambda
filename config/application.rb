fe497ca0c8ff766ea0d71e1764ecbbcedd7c1033
julio cabrera <juliocabrera820@gmail.com>
2021-04-10 12:41:17 -0500

    build: load presenter and repository paths

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,8 @@ module BackendRambda
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-
+    config.autoload_paths += Dir[Rails.root.join('app', 'repositories')]
+    config.autoload_paths += Dir[Rails.root.join('app', 'presenters')]
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files

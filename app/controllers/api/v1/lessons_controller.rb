1198b99238b701388cd74c913456e4f36d438e97
julio cabrera <juliocabrera820@gmail.com>
2021-04-11 18:05:50 -0500

    feat: add lessons controller

---
+++ app/controllers/api/v1/lessons_controller.rb
@@ -0,0 +1,37 @@
+module Api
+  module V1
+    class LessonsController < ApplicationController
+      def create
+        if LessonsRepository.new.create(lesson_params, params[:course_id])
+          render json: { message: 'lesson created successfully' }, status: :created
+        else
+          render json: lesson.error, status: :unprocessable_entity
+        end
+      end
+
+      def show
+        lesson = LessonsRepository.new.show(params[:id])
+        render json: LessonPresenter.new(lesson).as_json, status: :ok
+      end
+
+      def update
+        if LessonsRepository.new.update(params[:id], lesson_params)
+          render json: { message: 'Lesson has been successfully updated' }, status: :ok
+        else
+          render json: { message: 'Lesson could not be updated' }, status: :unprocessable_entity
+        end
+      end
+
+      def destroy
+        LessonsRepository.new.delete(params[:id])
+        head :no_content
+      end
+
+      private
+
+      def lesson_params
+        params.except(:format).permit(:lesson_number, :title, :description, :video_url).to_h
+      end
+    end
+  end
+end

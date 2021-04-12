cee868278a4536ead9669182ecd531a12a66b971
julio cabrera <juliocabrera820@gmail.com>
2021-04-11 22:09:19 -0500

    validate presence of price attribute and add relationship

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,5 +1,6 @@
 class Course < ApplicationRecord
-  validates :name, :description, :duration, presence: true
+  validates :name, :description, :duration, :price, presence: true
   has_one_attached :featured_image
   has_many :lessons, dependent: :destroy
+  has_many :sales
 end

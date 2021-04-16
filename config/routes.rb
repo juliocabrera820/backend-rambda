3a54f0e8c1a12f8b5870440c7f3b6a02dce75a62
julio cabrera <juliocabrera820@gmail.com>
2021-04-16 13:33:27 -0500

    add authentication routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
+      get '/authentication/sign_up', to: 'authentication#sign_up'
+      get '/authentication/sign_in', to: 'authentication#sign_in'
       resources :courses do
         resources :lessons
       end
-      resources :customers
+      resources :users do
+        resources :courses
+      end
       resources :sales, except: [:update, :destroy]
       resources :topics
     end

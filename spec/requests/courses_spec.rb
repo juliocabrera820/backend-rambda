46e4f1a29334ac1f8f5df6161577bfb6fad535c3
julio cabrera <juliocabrera820@gmail.com>
2021-04-21 12:03:09 -0500

    group tests

--- spec/requests/courses_spec.rb
+++ spec/requests/courses_spec.rb
@@ -1,9 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe 'Courses', type: :request do
-  describe 'GET /courses' do
+  before(:all) do
+    post '/api/v1/authentication/sign_up',
+         params: { name: 'laura', email: 'laura@gmail.com', password: '123', role: 'instructor' }
+  end
+  describe 'GET /users/1/courses' do
     it 'returns a 401 due to the authorization header is missing' do
-      get '/api/v1/courses'
+      get '/api/v1/users/1/courses'
       expect(response).to have_http_status(:unauthorized)
     end
     it 'returns a 403 due to authorization header is invalid' do
@@ -11,16 +15,15 @@ RSpec.describe 'Courses', type: :request do
       expect(response).to have_http_status(:forbidden)
     end
     it 'returns an empty array' do
-      post '/api/v1/authentication/sign_up', params: { name: 'lulu', email: 'lulu@gmail.com', password: '123' }
-      post '/api/v1/authentication/sign_in', params: { email: 'lulu@gmail.com', password: '123' }
+      post '/api/v1/authentication/sign_in', params: { email: 'laura@gmail.com', password: '123' }
       token = response.body
       get '/api/v1/users/1/courses', headers: { Authorization: "Bearer #{token}" }
       expect(response).to have_http_status(:success)
       expect(JSON.parse(response.body).size).to eq(0)
     end
+  end
+  describe 'POST /users/1/courses' do
     it 'returns a new course' do
-      post '/api/v1/authentication/sign_up',
-           params: { name: 'laura', email: 'laura@gmail.com', password: '123', role: 'instructor' }
       post '/api/v1/authentication/sign_in', params: { email: 'laura@gmail.com', password: '123' }
       token = response.body
       post '/api/v1/users/1/courses', headers: { Authorization: "Bearer #{token}" },

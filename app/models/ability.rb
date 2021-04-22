5d80d8909170302a5c23388eb071ecafc2bbdc6d
julio cabrera <juliocabrera820@gmail.com>
2021-04-22 00:48:46 -0500

    add more permissions

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,11 +5,14 @@ class Ability
 
   def initialize(user)
     if user.admin?
-      can :manage, :all
+      can :manage, Topic
+      can :update, Course
     elsif user.instructor?
       can :manage, Course
+      can :manage, Lesson
     elsif user.user?
       can :read, Course
+      can [:create, :read], Sale
     end
   end
 end

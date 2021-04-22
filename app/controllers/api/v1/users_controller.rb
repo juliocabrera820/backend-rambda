3e3b93ae26479a9c51044e6760cf0185660b3548
julio cabrera <juliocabrera820@gmail.com>
2021-04-22 00:47:26 -0500

    remove unneeded methods

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,28 +1,13 @@
 module Api
   module V1
     class UsersController < ApplicationController
-      rescue_from ActiveRecord::RecordNotUnique, with: :not_unique
-
-      def index
-        users = UsersRepository.new.all
-        render json: UsersPresenter.new(users).as_json, status: :ok
-      end
-
-      def create
-        if UsersRepository.new.create(user_params)
-          render json: { message: 'user created successfully' }, status: :created
-        else
-          render json: course.error, status: :unprocessable_entity
-        end
-      end
-
       def show
         customer = UsersRepository.new.show(params[:id])
         render json: UserPresenter.new(customer).as_json, status: :ok
       end
 
       def update
-        if CoursesRepository.new.update(params[:id], course_params)
+        if UsersRepository.new.update(params[:id], user_params)
           render json: { message: 'User has been successfully updated' }, status: :ok
         else
           render json: { message: 'User could not be updated' }, status: :unprocessable_entity
@@ -38,10 +23,6 @@ module Api
 
       def user_params
         params.permit(:name, :email, :password)
-      end
-
-      def not_unique
-        render json: { message: 'email must be unique' }, status: :unprocessable_entity
       end
     end
   end

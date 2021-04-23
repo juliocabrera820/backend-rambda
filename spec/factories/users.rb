0bc7d060485a75f110a9b8e4157a53d3727a0e43
julio cabrera <juliocabrera820@gmail.com>
2021-04-22 19:07:41 -0500

    create different users

---
+++ spec/factories/users.rb
@@ -0,0 +1,19 @@
+FactoryBot.define do
+  factory :user, class: User do
+    name { 'lulu' }
+    email { 'lulu@gmail.com' }
+    password { '123' }
+  end
+  factory :instructor, class: User do
+    name { 'floor' }
+    email { 'floor@gmail.com' }
+    password { '1234' }
+    role { 'instructor' }
+  end
+  factory :diego, class: User do
+    name { 'diego' }
+    email { 'diego@gmail.com' }
+    password { '12345' }
+    role { 'admin' }
+  end
+end

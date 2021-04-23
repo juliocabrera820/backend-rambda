177ea1cef5292ffaa8c1449183ea5c6ece8611a5
julio cabrera <juliocabrera820@gmail.com>
2021-04-22 19:04:32 -0500

    remove unneeded attribute

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,5 +1,4 @@
 class Lesson < ApplicationRecord
-  validates :lesson_number, :title, :description, :video_url, presence: true
-  validates :lesson_number, numericality: { only_integer: true }
+  validates :title, :description, :video_url, presence: true
   belongs_to :course
 end

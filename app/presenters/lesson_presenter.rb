88eed0ae95ab8b5e43ca2b7026225b07ce3beff1
julio cabrera <juliocabrera820@gmail.com>
2021-04-22 19:05:48 -0500

    remove unneeded attribute

--- app/presenters/lesson_presenter.rb
+++ app/presenters/lesson_presenter.rb
@@ -6,7 +6,6 @@ class LessonPresenter
   def as_json
     {
       id: lesson.id,
-      lesson_number: lesson.lesson_number,
       title: lesson.title,
       description: lesson.description,
       video_url: lesson.video_url

12091cdf32e8dbe227ed072ce949ca1d91bd436c
julio cabrera <juliocabrera820@gmail.com>
2021-04-22 19:43:42 -0500

    add access token method

--- spec/shared_contexts.rb
+++ spec/shared_contexts.rb
@@ -10,6 +10,10 @@ RSpec.shared_context 'instructor sign up request' do
   end
   image_path = Rails.root.join('spec', 'support', 'attachments', 'gem.jpg')
   let(:image) { Rack::Test::UploadedFile.new(image_path, 'image/jpg') }
+  def access_token
+    post '/api/v1/authentication/sign_in', params: { email: 'floor@gmail.com', password: '1234' }
+    response.body
+  end
 end
 
 RSpec.shared_context 'admin sign up request' do

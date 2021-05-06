e66c58b8bf1affd6ca3795c0d410fb76aa76004c
julio cabrera <juliocabrera820@gmail.com>
2021-05-06 09:57:59 -0500

    show avatar

--- app/presenters/user_presenter.rb
+++ app/presenters/user_presenter.rb
@@ -1,17 +1,19 @@
 class UserPresenter
   def initialize(user)
     @user = user
+    @user_decorator = UserDecorator.new(user)
   end
 
   def as_json
     {
       id: user.id,
       name: user.name,
-      email: user.email
+      email: user.email,
+      avatar: user_decorator.image_url
     }
   end
 
   private
 
-  attr_reader :user
+  attr_reader :user, :user_decorator
 end
